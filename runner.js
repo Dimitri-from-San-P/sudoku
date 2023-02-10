7eddbc336b867ca8d9de5e95994a5557585042d5
Dimitri Sukhoff <sukhovdk@gmail.com>
2023-02-10 12:24:43 +0300

    Create branch dev

--- runner.js
+++ runner.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 // Подключить функцию readAndSolve из соответствующего файла.
 const readAndSolve = require('./readAndSolve');
 
-// Все судоку для решения доступны в файле puzzles.txt.
+// Все судоку для решения доступны в файле puzzles.txt..
 // Прочесть файл puzzles.txt в кодировке 'utf-8' и передать его содержимое в функцию readAndSolve.
 fs.readFile(
   './puzzles.txt',
