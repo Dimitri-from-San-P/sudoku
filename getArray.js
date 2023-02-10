b591219f38fc876455f9a4e92cf26721768d67de
ivanM-create <mikhaylyutin.ivan@gmail.com>
2023-02-10 15:55:57 +0300

    create getArray

---
+++ getArray.js
@@ -0,0 +1,15 @@
+// const puzzle = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'
+
+function getArray(puzzle) {
+  const arrFromStr = puzzle.split('');
+  const res = [];
+  for (let i = 0; i < arrFromStr.length; i += 9) {
+    const devider = arrFromStr.slice(i, i + 9);
+    res.push(devider);
+  }
+  console.table(res);
+  }
+
+  getArray(puzzle)
+
+
